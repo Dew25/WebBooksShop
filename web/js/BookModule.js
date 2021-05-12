ef1dc01befba509f5db76958fa313ac8a5586cf2
Dew25 <dew25@ivkhk.ee>
2021-05-12 12:35:18 +0300

    Реализован метод добавления книги с файлами обложки и текста

--- web/js/BookModule.js
+++ web/js/BookModule.js
@@ -1,7 +1,90 @@
 class BookModule{
   printAddBookForm(){
     document.getElementById('context').innerHTML=
-    ``
+    `<h3 class="w-100 text-center my-5 ">Добавить новую книгу</h3>
+    <form id="bookForm" method="POST" enctype="multipart/form-data">
+      <div class="row w-50 my-2 mx-auto">
+        <div class="col-4 text-end">
+            Название книги 
+        </div>
+        <div class="col-8 text-start ">
+          <input class="w-100" type="text" name="name">
+        </div>
+      </div>
+      <div class="row w-50 my-2 mx-auto">
+        <div class="col-4 text-end">
+          Автор книги 
+        </div>
+        <div class="col-8 text-start">  
+          <input class="col-8" type="text" name="author">
+        </div>
+      </div>
+      <div class="row w-50 my-2 mx-auto">
+        <div class="col-4 text-end">   
+            Год издания книги 
+        </div>
+        <div class="col-8 text-start">  
+          <input class="col-4" type="text" name="publishedYear">
+        </div>
+      </div>
+      <div class="row w-50 my-2 mx-auto">
+        <div class="col-4 text-end">   
+            ISBN: 
+        </div>
+        <div class="col-8 text-start">  
+          <input class="col-8" type="text" name="isbn">
+        </div>
+      </div>
+      <div class="row w-50 my-2 mx-auto">
+        <div class="col-4 text-end">   
+            Цена: 
+        </div>
+        <div class="col-8 text-start">  
+          <input class="col-4" type="text" name="price">
+        </div>
+      </div>
+      <div class="row w-50 my-2 mx-auto">
+        <div class="col-4 text-end">
+            Загрузите обложку 
+        </div>
+        <div class="col-8 text-start">     
+            <input class="form-control col" type="file" name="file" id="file-cover">
+        </div>
+      </div>
+      <div class="row w-50 my-2 mx-auto">
+        <div class="col-4 text-end">
+            Текст книги 
+        </div>
+        <div class="col-8 text-start">     
+          <input class="form-control" type="file" name="file" id="file-text">
+        </div>
+      </div>
+      <div class="row w-50 my-2 mx-auto">
+        <div class="col-4 text-end">
+              
+        </div>
+        <div class="col-8 text-start mt-3">     
+          <input class="w-50 bg-primary text-white" type="submit" name="submit" value="Добавить">
+        </div>
+      </div>
+    </form>`;
+    document.getElementById('bookForm').onsubmit = function(e){
+      e.preventDefault();
+      bookModule.createBook();
+    }
+  }
+  async createBook(){
+    let response = await fetch('createBookJson',{
+      method: 'POST',
+      body: new FormData(document.getElementById('bookForm'))
+    })
+    if((response).ok){
+      const result = await response.json();
+      document.getElementById('info').innerHTML = result.info;
+
+    }else{
+      document.getElementById('info').innerHTML='Ошибка сервера';
+    }
   }
 }
 const bookModule = new BookModule();
